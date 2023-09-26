590108ebeaa63ea7776c8ac4f5f69ff01badf78f
Evanny254 <evancherry254@gmail.com>
2023-09-26 07:48:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+const tutorialNames = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(tutorialNames) {
+  if (!tutorialNames) {
+    return [
+  'What Does The This Keyword Mean?',
+  'What Is The Constructor OO Pattern?',
+  'Implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What Is NaN And How Can We Check For It',
+  'What Is The Difference Between StopPropagation And PreventDefault?',
+  'Immutable State And Pure Functions',
+  'What Is The Difference Between == And ===?',
+  'What Is The Difference Between Event Capturing And Bubbling?',
+  'What Is JSONP?'
+    ];
+  }
+  return tutorialNames.map(function(name) {
+    return name.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
+const titleCasedTutorialNames = titleCased(tutorialNames);
+
+console.log(titleCasedTutorialNames);
